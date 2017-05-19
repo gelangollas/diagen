0b9e7a5a9948520e41d9dbc2c3f20b7326a5720f
Gelangollas <gelangollas@gmail.com>
2017-05-19 19:45:15 +0700

    Extended grammar for components exraction.

--- application/diagen/utils/extraction/tomita/grammar.cxx
+++ application/diagen/utils/extraction/tomita/grammar.cxx
@@ -8,30 +8,56 @@ Delimiter -> Comma | Hyphen | Colon;
 ComponentPointer -> Word<gram="APRO"> | Word<kwtype="APRO">;
 
 //ключевое слово - тип компонента
-ComponentDeclaration -> Word<kwtype="тип_компонента"> interp (Component.Type);
+ComponentDeclaration -> Word<kwtype="тип_компонента", gram="sg"> interp (Component.Type);
+//ключевое слово - тип компонента во множественном числе
+ComponentDeclarationPlural -> Word<kwtype="тип_компонента", gram="pl"> interp (Component.Type);
 
 //слова, которые не склоняются и разделители
-DescriptionWordCommonPart -> Word<gram="PART"> | Word<gram="PR"> | Word<gram="CONJ"> | Word<gram="INTJ">;
-
-DescriptionWord1 -> Word<gram="gen"> | DescriptionWordCommonPart;
-DescriptionWord2 -> Word<gram="nom"> | Word<gram="acc"> | DescriptionWordCommonPart;
+ServicePartsOfSpeech -> Word<gram="PART"> | Word<gram="PR"> | Word<gram="CONJ"> | Word<gram="INTJ">;
+//слова, используемые для описания компонента
+DescriptionWord1NotService -> Word<gram="gen", kwtype=~"тип_компонента">;
+DescriptionWord1 -> DescriptionWord1NotService | ServicePartsOfSpeech;
+DescriptionWord2NotService -> Word<gram="nom", kwtype=~"тип_компонента"> | Word<gram="acc", kwtype=~"тип_компонента">;
+DescriptionWord2 -> DescriptionWord2NotService | ServicePartsOfSpeech;
 
 //описание компонента
-ComponentDescription -> DescriptionWord1+;
+ComponentDescription -> DescriptionWord1NotService DescriptionWord1* DescriptionWord1NotService | DescriptionWord1* DescriptionWord1NotService | DescriptionWord1NotService;
 //описание действия, выполняемого компонентом
-ComponentDescription -> Word<gram="partcp,V"> DescriptionWord2+;
+ComponentDescription -> Word<gram="partcp,V"> DescriptionWord2* DescriptionWord2NotService;
 //характеристика компонента
 ComponentCharacteristic -> Adj<gram="sg"> interp (Component.Description);
 
-ComponentName -> AnyWord<lat, h-reg1>;
+ComponentName -> AnyWord<lat, h-reg1>+ | AnyWord<h-reg2>;
+
+
+/*
+Далее идут шаблоны для описания компонентов 
+*/
 
 //[указатель][компонент]
-Component -> ComponentPointer interp (Component.Pointer=true; Component.Description) ComponentDeclaration {weight = 1.1};
+ComponentNoName -> ComponentPointer interp (Component.Pointer=true; Component.Description) ComponentDeclaration {weight = 1.1};
 //[компонент][описание компонента] и [компонент][действие][описание действия]
-Component -> ComponentDeclaration (Delimiter) ComponentDescription interp (Component.Description);
+ComponentNoName -> ComponentDeclaration (Delimiter) ComponentDescription interp (Component.Description);
 //[характеристика компонента][компонент]
-Component -> ComponentCharacteristic ComponentDeclaration;
+ComponentNoName -> ComponentCharacteristic ComponentDeclaration;
+
+Component -> ComponentNoName;
+
+//[компонент][название компонента на английском]
+Component -> ComponentDeclaration ComponentName interp (Component.Name);
 //[название компонента на английском]
-Component -> ComponentName interp (Component.Name; Component.Type="default");
+Component -> ComponentName interp (Component.Name);
+//[название компонента на английском][-][описание компонента]
+Component -> ComponentName interp (Component.Name) (Delimiter) ComponentNoName;
+//[описание компонента][-][название компонента на английском]
+Component -> ComponentNoName (Delimiter) ComponentName interp (Component.Name);
+
+
+EnumeratedComponent -> Component | ComponentDescription interp (Component.Description) | ComponentName interp (Component.Name; Component.Description=" ");
+ComponentsEnumerationEnd -> Comma EnumeratedComponent | "и" EnumeratedComponent;
+ComponentsEnumerationPart -> Comma EnumeratedComponent;
+ComponentsEnumeration -> EnumeratedComponent ComponentsEnumerationPart* ComponentsEnumerationEnd;
+Components -> ComponentDeclarationPlural (Hyphen) (Colon) ComponentsEnumeration;
 
 S -> Component;
+S -> Components;
